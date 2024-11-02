81aefc5bafca1cd9e271b186b7b6d7317113d4a6
void2610 <izumisan2610@gmail.com>
2024-11-02 20:27:02 +0900

    change wall sprite

--- Assets/Scripts/Merge/MergeWall.cs
+++ Assets/Scripts/Merge/MergeWall.cs
@@ -9,15 +9,24 @@ public class MergeWall : MonoBehaviour
     private GameObject rightWall;
     [SerializeField]
     private GameObject floor;
+
+    [SerializeField] private RectTransform image;
     [SerializeField]
     private float xOffset = 0.0f;
 
     public float WallWidth { get; private set; } = 1.5f;
     private float defaultY = 0.0f;
+    private float minWidth = 80.0f;
+    private float maxWidth = 365.0f;
 
     public void SetWallWidth(float width)
     {
         WallWidth = width;
+        // 1.5から10をminWidthからmaxWidthの範囲に収める
+        float ratio = (width - 1.5f) / 8.5f;
+        Debug.Log(ratio);
+        image.sizeDelta = new Vector2(minWidth + (maxWidth - minWidth) * ratio, image.sizeDelta.y);
+        
         leftWall.transform.DOMoveX(-WallWidth / 2 + xOffset, 0.5f);
         rightWall.transform.DOMoveX(WallWidth / 2 + xOffset, 0.5f);
         floor.transform.DOScaleX(WallWidth, 0.5f);
