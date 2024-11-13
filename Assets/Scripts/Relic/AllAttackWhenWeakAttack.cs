30a0a169384c4370a5f80cf75d1159f0b986b87d
void2610 <izumisan2610@gmail.com>
2024-11-13 10:01:20 +0900

    add all attack relic

---
+++ Assets/Scripts/Relic/AllAttackWhenWeakAttack.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using R3;
+
+public class AllAttackWhenWeakAttack : MonoBehaviour, IRelicBehavior
+{
+    private IDisposable disposable;
+    private RelicUI ui;
+    public void ApplyEffect(RelicUI relicUI)
+    {
+        ui = relicUI;
+        disposable = EventManager.OnPlayerAttack.Subscribe(Effect);
+    }
+
+    public void RemoveEffect()
+    {
+        disposable.Dispose();
+    }
+    
+    private void Effect(Unit _)
+    {
+        var x = EventManager.OnPlayerAttack.GetValue();
+        if (x.Item1 < 10)
+        {
+            EventManager.OnPlayerAttack.SetValue((x.Item1, true));
+            ui.ActivateUI();
+        }
+    }
+}
