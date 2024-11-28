434eb34e7fa43506d9f8484e224d983a7387bc1d
void2610 <izumisan2610@gmail.com>
2024-11-28 10:56:03 +0900

    add torch relic

---
+++ Assets/Scripts/Relic/DoubleAttackWhenLowHealth.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using R3;
+
+public class DoubleAttackWhenLowHealth : MonoBehaviour, IRelicBehavior
+{
+    private IDisposable disposable;
+    private RelicUI ui;
+    public void ApplyEffect(RelicUI relicUI)
+    {
+        ui = relicUI;
+        disposable = EventManager.OnPlayerAttack.Subscribe(Effect);
+    }
+
+    public void RemoveEffect()
+    {
+        disposable.Dispose();
+    }
+    
+    private void Effect(Unit _)
+    {
+        if (GameManager.Instance.player.health.Value <= 20)
+        {
+            var atk = EventManager.OnPlayerAttack.GetValue();
+            EventManager.OnPlayerAttack.SetValue((atk.Item1 * 2, atk.Item2 * 2));
+            ui?.ActivateUI();
+        }
+    }
+}
