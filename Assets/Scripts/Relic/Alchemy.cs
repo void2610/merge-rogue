fa38855a31c553a00cb37d928addacfeb053ccdc
void2610 <izumisan2610@gmail.com>
2024-12-11 02:30:48 +0900

    add alchemy relics

---
+++ Assets/Scripts/Relic/Alchemy.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using R3;
+
+public class Alchemy : MonoBehaviour, IRelicBehavior
+{
+    private IDisposable disposable;
+    private RelicUI ui;
+    public void ApplyEffect(RelicUI relicUI)
+    {
+        disposable = EventManager.OnPlayerAttack.Subscribe(Effect).AddTo(this);
+        ui = relicUI;
+
+        Effect(Unit.Default);
+    }
+
+    public void RemoveEffect()
+    {
+        disposable?.Dispose();
+    }
+    
+    private void Effect(Unit _)
+    {
+        var coin = GameManager.Instance.coin.Value;
+        var e = GameManager.Instance.enemyContainer.GetCurrentEnemyCount();
+        var x = EventManager.OnPlayerAttack.GetValue();
+
+        // 消費するコインが存在し、単体攻撃力が1以上、敵が2体以上いる場合
+        if (coin >= 10 && x.Item1 > 0 && e >= 2)
+        {
+            GameManager.Instance.SubtractCoin(10);
+            EventManager.OnPlayerAttack.SetValue((0, x.Item1 + x.Item2));
+            ui?.ActivateUI();
+        }
+    }
+    
+    private void OnDestroy()
+    {
+        RemoveEffect();
+    }
+}
---
+++ Assets/Scripts/Relic/ReverseAlchemy.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using R3;
+
+public class ReverseAlchemy : MonoBehaviour, IRelicBehavior
+{
+    private IDisposable disposable;
+    private RelicUI ui;
+    public void ApplyEffect(RelicUI relicUI)
+    {
+        disposable = EventManager.OnPlayerAttack.Subscribe(Effect);
+        ui = relicUI;
+
+        Effect(Unit.Default);
+    }
+
+    public void RemoveEffect()
+    {
+        disposable?.Dispose();
+    }
+    
+    private void Effect(Unit _)
+    {
+        var x = EventManager.OnPlayerAttack.GetValue();
+        ui?.ActivateUI();
+    }
+}
