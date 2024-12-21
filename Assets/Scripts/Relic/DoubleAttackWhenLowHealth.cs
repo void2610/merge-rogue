2ab7ed7317efbf8930f9bf3c9b5e892c44a28da8
void2610 <izumisan2610@gmail.com>
2024-12-21 20:34:21 +0900

    fix

--- Assets/Scripts/Relic/DoubleAttackWhenLowHealth.cs
+++ Assets/Scripts/Relic/DoubleAttackWhenLowHealth.cs
@@ -20,7 +20,7 @@ public class DoubleAttackWhenLowHealth : MonoBehaviour, IRelicBehavior
     
     private void Effect(Unit _)
     {
-        if (GameManager.Instance.player.health.Value <= GameManager.Instance.player.maxHealth.Value * 0.8f)
+        if (GameManager.Instance.player.health.Value <= GameManager.Instance.player.maxHealth.Value * 0.2f)
         {
             var atk = EventManager.OnPlayerAttack.GetValue();
             EventManager.OnPlayerAttack.SetValue((atk.Item1 * 2, atk.Item2 * 2));
