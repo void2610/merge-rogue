ce742832f32eb4b761510252b2a1218908dbf1f4
void2610 <izumisan2610@gmail.com>
2025-02-09 13:40:45 +0900

    impl tutorial

---
+++ Assets/Scripts/UI/TutorialPanel.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialPanel : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> pages;
+    [SerializeField] private Button nextButton;
+    [SerializeField] private Button previousButton;
+    
+    private int _currentPage = 0;
+    private GameObject _currentActivePage;
+    private RectTransform _rectTransform;
+
+    private void NextPage()
+    {
+        if (_currentPage >= pages.Count - 1) return;
+        ChangePage(_currentPage + 1);
+    }
+    
+    private void PreviousPage()
+    {
+        if (_currentPage <= 0) return;
+        ChangePage(_currentPage - 1);
+    }
+    
+    private void ChangePage(int page)
+    {
+        if(_currentActivePage) Destroy(_currentActivePage);
+        _currentActivePage = Instantiate(pages[page], this.transform);
+        _currentPage = page;
+    }
+
+    private void Awake()
+    {
+        ChangePage(0);
+        
+        nextButton.onClick.AddListener(NextPage);
+        previousButton.onClick.AddListener(PreviousPage);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            NextPage();
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            PreviousPage();
+        }
+    }
+}
