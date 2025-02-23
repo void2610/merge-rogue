b1a52f156b3643e24d01b802e59234329eda878b
void2610 <izumisan2610@gmail.com>
2025-02-23 12:05:14 +0900

    impl clear ball

---
+++ Assets/Scripts/Ball/ClearBall.cs
@@ -0,0 +1,41 @@
+using System;
+
+public class ClearBall : BallBase
+{
+    protected override void Effect(BallBase other)
+    {
+        base.Effect(other);
+
+        // プレイヤーに付与された全ての状態異常を5スタック減少させる
+        if (Level == 0)
+        {
+            foreach(StatusEffectType effect in Enum.GetValues(typeof(StatusEffectType)))
+            {
+                StatusEffectFactory.RemoveStatusEffectFromPlayer(effect, 5);
+            }
+        }
+        // 全ての敵に付与された全ての状態異常を5スタック減少させる
+        else if (Level == 1)
+        {
+            foreach(StatusEffectType effect in Enum.GetValues(typeof(StatusEffectType)))
+            {
+                foreach(var enemy in EnemyContainer.Instance.GetAllEnemies())
+                {
+                    StatusEffectFactory.RemoveStatusEffect(enemy, effect, 5);
+                }
+            }
+        }
+        // プレイヤーと全ての敵に付与された全ての状態異常を5スタック減少させる
+        else if (Level == 2)
+        {
+            foreach(StatusEffectType effect in Enum.GetValues(typeof(StatusEffectType)))
+            {
+                StatusEffectFactory.RemoveStatusEffectFromPlayer(effect, 5);
+                foreach(var enemy in EnemyContainer.Instance.GetAllEnemies())
+                {
+                    StatusEffectFactory.RemoveStatusEffect(enemy, effect, 5);
+                }
+            }
+        }
+    }
+}
