7c2125e39d2e59c416703a6ec4ffd30d62756f3f
void2610 <izumisan2610@gmail.com>
2025-03-01 12:20:19 +0900

    impl credit link

--- Assets/Scripts/UI/Title/UpdateCredit.cs
+++ Assets/Scripts/UI/Title/UpdateCredit.cs
@@ -1,7 +1,11 @@
 using TMPro;
 using UnityEngine;
 
-public class UpdateCredit : MonoBehaviour
+using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class UpdateCredit : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] private TextAsset textAsset;
     [SerializeField] private TextMeshProUGUI text;
@@ -11,12 +15,42 @@ public class UpdateCredit : MonoBehaviour
     {
         var textData = textAsset.text;
         textData = "\n\n\n\n" + textData;
-        text.text = textData;
+        
+        // URL をリンクとして扱えるようにする
+        text.text = ConvertUrlsToLinks(textData);
         
         // テキストのPreferred Valuesを取得
         var preferredHeight = text.GetPreferredValues().y;
 
         // Contentの高さをPreferred Heightに合わせる
         content.sizeDelta = new Vector2(content.sizeDelta.x, preferredHeight);
+    }
+
+    /// <summary>
+    /// クリック時にリンク部分を判定し、URLを開く
+    /// </summary>
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Click");
+        var linkIndex = TMP_TextUtilities.FindIntersectingLink(text, eventData.position, Camera.main);
+        if (linkIndex != -1)
+        {
+            Debug.Log("Link Clicked");
+            var linkInfo = text.textInfo.linkInfo[linkIndex];
+            var url = linkInfo.GetLinkID();
+            Application.OpenURL(url);
+        }
+    }
+
+    /// <summary>
+    /// テキスト内のURLをTextMeshProのリンク形式に変換
+    /// </summary>
+    private string ConvertUrlsToLinks(string textData)
+    {
+        return System.Text.RegularExpressions.Regex.Replace(
+            textData,
+            @"(http[s]?:\/\/[^\s]+)",
+            "<link=\"$1\"><u>$1</u></link>"
+        );
     }
 }
