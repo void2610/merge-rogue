8ab8e68edf056090cb5e1eaf405c5c9eac2c3df2
void2610 <izumisan2610@gmail.com>
2025-05-20 14:25:11 +0900

    fix ref of tutorial button

--- Assets/Scripts/UI/TutorialPanel.cs
+++ Assets/Scripts/UI/TutorialPanel.cs
@@ -8,20 +8,18 @@ using UnityEngine.UI;
 public class TutorialPanel : MonoBehaviour
 {
     [SerializeField] private List<GameObject> pages;
-    [SerializeField] private Button nextButton;
-    [SerializeField] private Button previousButton;
     
     private int _currentPage = 0;
     private GameObject _currentActivePage;
     private RectTransform _rectTransform;
 
-    private void NextPage()
+    public void NextPage()
     {
         if (_currentPage >= pages.Count - 1) return;
         ChangePage(_currentPage + 1);
     }
     
-    private void PreviousPage()
+    public void PreviousPage()
     {
         if (_currentPage <= 0) return;
         ChangePage(_currentPage - 1);
@@ -39,30 +37,10 @@ public class TutorialPanel : MonoBehaviour
         
         // ハイライトからウィンドウ表示できるように
         DescriptionWindow.Instance.AddTextToObservation(_currentActivePage);
-        
-        // previousButton.interactable = _currentPage > 0;
-        // nextButton.interactable = _currentPage < pages.Count - 1;
     }
 
     private void Start()
     {
         ChangePage(0);
-        
-        nextButton.onClick.AddListener(NextPage);
-        previousButton.onClick.AddListener(PreviousPage);
     }
-
-    // private void Update()
-    // {
-    //     if(!UIManager.Instance.IsEnableCanvasGroup("Tutorial")) return;
-    //     
-    //     if (InputProvider.Instance.Gameplay.RightMove.triggered)
-    //     {
-    //         NextPage();
-    //     }
-    //     else if (InputProvider.Instance.Gameplay.LeftMove.triggered)
-    //     {
-    //         PreviousPage();
-    //     }
-    // }
 }
